33b5806cb10140e906f9330bda6647a69a7902e3
Ashraf1551 <mdashrafgmhs@gmail.com>
2024-08-04 20:30:45 +0600

    From Ubuntu_1

---
+++ Institute/Module_Day/C++ for DSA/Module 01/4_1_setPrecise.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+
+int main() {
+    double a;
+    cin>>a;
+    cout<<fixed<<setprecision(5)<<a;
+
+    cout << endl;
+    return 0;
+}
